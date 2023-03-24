2a13ae4e9658b0a719e990c8725735fdab2b32c8
adila putri efilda <106298322+AdillaPutri@users.noreply.github.com>
2023-03-24 17:35:20 +0700

    aseng

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Hotel',
+      title: 'aseng',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
